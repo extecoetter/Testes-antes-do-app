a6d27361d130725a78821f3f5b322d026dc061d4
extecoetter <extecoetterrobson@gmail.com>
2026-03-17 20:28:25 -0300

    Add files via upload

---
+++ clientes/testador/config.js
@@ -0,0 +1,8 @@
+window.CLIENT_CONFIG = {
+  installer: {
+    nome: "Instalador Refrigerista",
+    whats: "(14) 99707-9999",
+    pix: "14 99707 9999"
+  },
+  logo: "/APPclimatizacao/clientes/testador/logo.png"
+};
